2151672da8de1793e43f719d7d6643bba1083b10
sgrazys <s.grazys@gmail.com>
2023-06-03 01:40:55 +0300

    add modal funcionality

--- script.js
+++ script.js
@@ -10,12 +10,15 @@ const busyHoursDOM = document.querySelector('.form__input--hours');
 const headlineDOM = document.querySelector('.headline');
 const dataHeaderDOM = document.querySelector('.data__header');
 const listDOM = document.querySelector('.list');
+const modalDOM = document.querySelector('.modal');
+const overlayDOM = document.querySelector('.overlay');
 
 // Buttons
 const btnAddBusyTime = document.querySelector('.btn--add');
 const btnShow = document.querySelector('.btn--show');
 const btnReset = document.querySelector('.btn--reset');
 const btnScopeDeadline = document.querySelector('.btn--add__scope__deadline');
+const btnCloseModal = document.querySelector('.btn--close-modal');
 
 // DATA
 const busyDates = []; // ATSIRANDA Pridedant busy date ir busy hours
@@ -25,6 +28,22 @@ const sleepingHours = 8;
 const today = new Date();
 let scope = 0;
 
+// Modal functions
+const closeModal = () => {
+	modalDOM.classList.add('hidden');
+	overlayDOM.classList.add('hidden');
+};
+
+const openModal = () => {
+	modalDOM.classList.remove('hidden');
+	overlayDOM.classList.remove('hidden');
+};
+
+const modalText = function (message) {
+	const modaltextDOM = document.querySelector('.modal__text');
+	modaltextDOM.innerHTML = message;
+};
+
 // Event listeners
 btnScopeDeadline.addEventListener('click', function (e) {
 	e.preventDefault();
@@ -32,11 +51,18 @@ btnScopeDeadline.addEventListener('click', function (e) {
 	const deadline = deadlineDOM.value;
 
 	// Validation of scope
-	if (scope === 0) return alert('Please add valid scope of project.');
-	if (scope < 0) return alert('Project scope can not be negative.');
+	if (scope === 0 || scope < 0) {
+		modalText('Please enter valid project scope.');
+		openModal();
+		return;
+	}
 
 	// Validation of deadline
-	if (deadline === '') return alert('Please choose project deadline');
+	if (deadline === '') {
+		modalText('Please choose project deadline.');
+		openModal();
+		return;
+	}
 
 	// days until deadline
 	const daysLeft = Math.floor(
@@ -44,11 +70,11 @@ btnScopeDeadline.addEventListener('click', function (e) {
 			(1000 * 60 * 60 * 24)
 	);
 
-	if (new Date(deadline).getTime() <= today.getTime())
-		return alert(
-			'Your project deadline cannot be current day or previous days! '
-		);
-
+	if (new Date(deadline).getTime() < today.getTime()) {
+		modalText('Your project deadline cannot be any previous days.');
+		openModal();
+		return;
+	}
 	for (let i = 0; i < daysLeft; i++) {
 		const nextDay = new Date(today);
 		nextDay.setDate(today.getDate() + 1 + i);
@@ -73,29 +99,58 @@ btnAddBusyTime.addEventListener('click', (e) => {
 	const busyHour = +busyHoursDOM.value;
 
 	// Validations
-	if (busyHour > hoursInDay - sleepingHours)
-		return alert(
-			"Ouuuu nou you don't have so many available hours. Max - 16 , as you are sleeping 8 hours per day :)"
+	if (busyHour > hoursInDay - sleepingHours) {
+		modalText(
+			"You don't have as many available hours. The maximum is 16 hours, considering that you sleep for 8 hours per day."
 		);
+		openModal();
+		return;
+	}
 
-	if (busyDate === '') return alert('Please add your busy date.');
-	if (busyHour === 0)
-		return alert('Please add your busy hours amount on that day.');
+	if (busyDate === '') {
+		modalText('Please add your busy date.');
+		openModal();
+		return;
+	}
+	if (busyHour === 0) {
+		modalText(
+			'Please specify the number of hours you will be busy on that day.'
+		);
+		openModal();
+		return;
+	}
 
 	if (
 		new Date(busyDateDOM.value).getTime() >
 		new Date(deadlineDOM.value).getTime()
-	)
-		return alert('You pick busy date after project deadline. ');
+	) {
+		modalText(
+			'You have selected a busy date that falls after the project deadline.'
+		);
+		openModal();
+		return;
+	}
+
 	if (
 		new Date(busyDateDOM.value).getTime() ===
 		new Date(deadlineDOM.value).getTime()
-	)
-		return alert('This is your deadline date.');
-	if (new Date(busyDateDOM.value).getTime() < today.getTime())
-		return alert('Ooops, you can not to plan history :)');
+	) {
+		modalText("You have selected the project's deadline date.");
+		openModal();
+		return;
+	}
 
-	if (allDates.length === 0) return alert('Please define project deadline ');
+	if (new Date(busyDateDOM.value).getTime() < today.getTime()) {
+		modalText("Oops, you can't plan history.");
+		openModal();
+		return;
+	}
+
+	if (allDates.length === 0) {
+		modalText('Please provide the project deadline.');
+		openModal();
+		return;
+	}
 
 	const busy = {
 		date: busyDate,
@@ -109,7 +164,6 @@ btnAddBusyTime.addEventListener('click', (e) => {
 	sameDate.freeHours > busyHour
 		? (sameDate.freeHours -= busyHour)
 		: (sameDate.freeHours = 0);
-	// Update
 	// Reset values
 	busyDateDOM.value = busyHoursDOM.value = '';
 
@@ -127,12 +181,16 @@ btnAddBusyTime.addEventListener('click', (e) => {
 btnShow.addEventListener('click', function () {
 	const freeHours = allDates.reduce((t, curr) => t + curr.freeHours, 0);
 
-	if (freeHours < scope)
-		return alert(
-			`Neturi pakankamai laiko uzbaigti projekto. Tau truksta ${
-				scope - freeHours
-			} valandu!`
+	if (freeHours < scope) {
+		const missingHours = scope - freeHours;
+		modalText(
+			`Oops, you don't have enough time to complete your project. You need an additional ${missingHours} ${
+				missingHours === 1 ? 'hour' : 'hours'
+			}.`
 		);
+		openModal();
+		return;
+	}
 
 	// Share available hours per day
 	scope;
@@ -172,4 +230,18 @@ btnReset.addEventListener('click', function () {
 	busyDateDOM.value = '';
 	busyHoursDOM.value = '';
 	headlineDOM.innerHTML = "Let's Plan Project!";
+});
+
+btnCloseModal.addEventListener('click', function () {
+	closeModal();
+});
+
+document.addEventListener('keydown', function (e) {
+	if (
+		e.key === 'Escape' &&
+		!modalDOM.classList.contains('hidden') &&
+		!overlayDOM.classList.contains('hidden')
+	) {
+		closeModal();
+	}
 });
